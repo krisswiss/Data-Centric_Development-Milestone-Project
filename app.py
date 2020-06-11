1cf6f78b4c4e9dfd46441dd8f7042bf576cc49a1
krisswiss <chrisswiatek@hotmail.com>
2020-06-11 22:18:14 +0000

    app.py,requirements,Procfile, folder structure and environment var for password setup and tested

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME')
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/main')
+def results():
+    shoes = mongo.db.shoes.find()
+    return render_template("main.html", shoes=shoes)
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
